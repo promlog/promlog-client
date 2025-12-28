269c3d599f9f70ef8a38afd4468a5b39de1d6ac4
sohee <106158901+ccconac@users.noreply.github.com>
2025-12-29 00:02:11 +0900

    feat: Banner 컴포넌트 구현 (#15)
    * design: global.css 스타일 속성 추가
    
    *margin, padding default 0 설정
    *font-size 제거
    
    * design: NavigationBar 컴포넌트 padding 속성 제거
    
    *px-4 제거
    
    * feat: Banner 컴포넌트 구현

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -5,7 +5,7 @@ import { Logo } from '../Logo/Logo';
 const NavigationBar = () => {
   return (
     <header className="bg-white w-full border-b border-gray-200 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="max-w-7xl mx-auto">
         <div className="flex items-center justify-between h-16">
           <Logo.Basic />
           <div className="flex items-center gap-3">
