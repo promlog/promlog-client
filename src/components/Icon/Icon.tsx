f2fda769230f4267ddfbbc0f98be6203bb76e27d
ccconac <qwerpoiu91@naver.com>
2025-12-28 18:57:54 +0900

    feat: icon 컴포넌트 구현

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,11 @@
+import type { IconProps } from './Icon.types';
+import { iconMap, sizeMap } from './iconMap';
+
+export const Icon = ({ name, size = 'md', color, ...rest }: IconProps) => {
+  const pixelSize = sizeMap[size];
+  const IconComponent = iconMap[name];
+
+  if (!IconComponent) return null;
+
+  return <IconComponent width={pixelSize} height={pixelSize} style={{ color: '#000' }} {...rest} />;
+};
