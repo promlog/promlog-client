afd73f613513da644c65b9b148b1c17b3ded1fc4
sohee <106158901+ccconac@users.noreply.github.com>
2025-12-28 22:31:33 +0900

    feat: NavigationBar 컴포넌트 구현 (#10)
    * feat: NavigationBar 컴포넌트 구현
    
    * feat: button 컴포넌트에 icon 관련 prop 추가
    
    * design: button 컴포넌트 스타일 수정
    
    *gap 추가
    
    * style: size → iconSize prop 네이밍 변경
    
    *button size와 icon size prop 혼동 방지를 위함

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,23 @@
+import Button from '../Button/Button';
+import { Logo } from '../Logo/Logo';
+
+// TODO: 버튼 클릭 시 다이얼로그 표시
+const NavigationBar = () => {
+  return (
+    <header className="bg-white w-full border-b border-gray-200 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <Logo.Basic />
+          <div className="flex items-center gap-3">
+            <Button icon="addLine" iconSize="md">
+              프롬프트 등록
+            </Button>
+            <Button variant="solid">로그인</Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavigationBar;
