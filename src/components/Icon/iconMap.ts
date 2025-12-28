37d521f5cd743dfa81cebf018c1055bb75f5d676
ccconac <qwerpoiu91@naver.com>
2025-12-28 18:47:55 +0900

    feat: 명령어로 추가된 iconMap.ts

---
+++ src/components/Icon/iconMap.ts
@@ -0,0 +1,15 @@
+// 자동으로 생성된 파일입니다. 수정을 금지합니다.
+// 마지막 수정 일시 Sun Dec 28 2025 18:44:25 GMT+0900 (대한민국 표준시)
+
+import * as Icon from './generated/index.tsx';
+
+export const iconMap = {
+  addLine: Icon.AddLine,
+  bookmark: Icon.Bookmark,
+  circuit: Icon.Circuit,
+  copy: Icon.Copy,
+  link: Icon.Link,
+  logo: Icon.Logo,
+} as const;
+
+export type IconName = keyof typeof iconMap;
