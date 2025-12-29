ef2662750d5f9b7d1211b3f1ebfc94169a5c41ac
ccconac <qwerpoiu91@naver.com>
2025-12-29 22:11:36 +0900

    refactor: Button 컴포넌트 NavigationBar 외부로 이동
    *렌더링마다 생성 방지

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,16 +2,16 @@ import Button from '../Button/Button';
 import { Dialog } from './_components/Dialog';
 import { Logo } from '../Logo/Logo';
 
+const AddPromptButton = () => (
+  <Button icon="addLine" iconSize="md">
+    프롬프트 등록
+  </Button>
+);
+
+const LoginButton = () => <Button variant="solid">로그인</Button>;
+
 // TODO: 로그인 시 LoginButton 회원 정보 표시 버튼 (클릭 시 메뉴 표시) 교체 필요 → 회원 탈퇴 클릭 시 회원 탈퇴 다이얼로그 표시
 const NavigationBar = () => {
-  const AddPromptButton = () => (
-    <Button icon="addLine" iconSize="md">
-      프롬프트 등록
-    </Button>
-  );
-
-  const LoginButton = () => <Button variant="solid">로그인</Button>;
-
   return (
     <header className="bg-white w-full border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto">
