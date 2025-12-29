6ec11fdb28088ba04ded3a4d68e5e2e20f8554ae
ccconac <qwerpoiu91@naver.com>
2025-12-29 23:01:43 +0900

    fix: 잘못된 tailwind 클래스 수정
    *w-7xl → max-w-7xl 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className="flex w-full justify-center flex-col mx-auto bg-gray-50 h-full">
       <NavigationBar />
-      <main className="justify-center flex mx-auto w-7xl px-4 py-8">
+      <main className="justify-center flex mx-auto max-w-7xl px-4 py-8">
         <Outlet />
       </main>
     </div>
