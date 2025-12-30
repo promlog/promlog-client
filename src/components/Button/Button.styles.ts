b19577d992a97f17091dbb0e9c2dc5f22d95a6a6
ccconac <qwerpoiu91@naver.com>
2025-12-30 19:13:33 +0900

    feat: Button 컴포넌트 variant에 empty 추가
    *버튼 컴포넌트 확장

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,4 +1,5 @@
 export const buttonVariants = {
-  solid: `bg-gray-900 hover:bg-gray-800 transition-colors`,
-  primary: `bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700`,
+  solid: `bg-gray-900 hover:bg-gray-800 transition-colors text-white px-4`,
+  primary: `bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700 text-white px-4`,
+  empty: 'text-gray-600 hover:text-gray-900 transition-colors',
 };
