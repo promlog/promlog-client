5cf3c74acec38d8198b242c4218f427c479ac4f8
ccconac <qwerpoiu91@naver.com>
2025-12-30 19:17:32 +0900

    fix: 잘못된 import 경로 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import PromptListPage from './pages/PromptList/PromptList';
+import PromptListPage from './pages/PromptList/PromptListPage';
 import Layout from './components/Layout/Layout';
 
 const Router = () => {
