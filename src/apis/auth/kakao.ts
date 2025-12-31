f234c4e62d980d11f14ae47c3314df30104b7d01
ccconac <qwerpoiu91@naver.com>
2025-12-31 19:25:23 +0900

    rename: kakao.types.ts 네이밍 변경
    *kakao → auth

--- src/apis/auth/kakao.ts
+++ src/apis/auth/kakao.ts
@@ -1,5 +1,5 @@
 import { API } from '../../config/api';
-import type { KakaoLoginResponse } from './kakao.types';
+import type { KakaoLoginResponse } from './auth.types';
 
 export const postKakaoCode = async (code: string): Promise<KakaoLoginResponse> => {
   const { data } = await API.post<KakaoLoginResponse>('/api/auth/oauth/kakao/code', {
@@ -8,3 +8,4 @@ export const postKakaoCode = async (code: string): Promise<KakaoLoginResponse> =
 
   return data;
 };
+
