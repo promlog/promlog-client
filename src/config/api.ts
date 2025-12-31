4435216215ee7500e47c194064c1c924c78c696f
ccconac <qwerpoiu91@naver.com>
2025-12-31 19:23:09 +0900

    chore: axios interceptor 추가
    *api 요청 시 header에 액세스 토큰 싣어 보내기 위함

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { authStorage } from '../lib/authStorage';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -6,3 +7,17 @@ export const API = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
 });
+
+API.interceptors.request.use(
+  (config) => {
+    const token = authStorage.getAccessToken();
+
+    if (token) {
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
