5ef233c8e85b777184bb10600314a0d4e1e38a3a
ccconac <qwerpoiu91@naver.com>
2025-12-31 17:17:45 +0900

    feat: API BASE URL 상수 추가

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
