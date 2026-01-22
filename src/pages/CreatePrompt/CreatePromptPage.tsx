b6ae5653c5fd6956b148f553664b0a757d09c858
ccconac <qwerpoiu91@naver.com>
2026-01-22 18:27:27 +0900

    feat: 프롬프트 등록 페이지 UI 구현

---
+++ src/pages/CreatePrompt/CreatePromptPage.tsx
@@ -0,0 +1,20 @@
+import Button from '../../components/Button/Button';
+import CreatePromptForm from './_components/CreatePromptForm';
+
+const CreatePromptPage = () => {
+  return (
+    <div className="w-3xl">
+      <section className="w-full max-w-2xl bg-white rounded-xl border border-gray-200 overflow-hidden shadow-sm">
+        <header className="flex items-center justify-between p-6 border-b border-gray-200">
+          <h2 className="text-gray-900 text-lg">프롬프트 등록</h2>
+          <Button icon="close" variant="empty" aria-label="닫기" />
+        </header>
+        <div className="p-6">
+          <CreatePromptForm />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default CreatePromptPage;
