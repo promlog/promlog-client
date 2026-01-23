98054aeafff40ddce0a9ead7093be31fabb7e73a
ccconac <qwerpoiu91@naver.com>
2026-01-24 01:46:30 +0900

    feat: 프롬프트 상세 페이지 설명 추가

--- src/pages/PromptDetail/PromptDetailPage.tsx
+++ src/pages/PromptDetail/PromptDetailPage.tsx
@@ -43,6 +43,10 @@ const PromptDetailPage = () => {
           date={createdAt}
           writer={author.name}
         />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-gray-800 text-xl">💬 프롬프트 설명</h2>
+          <p className="text-gray-700 leading-relaxed">{description}</p>
+        </div>
         <PromptContentBox description={prompt} />
       </div>
     </div>
