443caffe279bc2af4fd7c4c763c54814bb5fc619
ccconac <qwerpoiu91@naver.com>
2026-01-24 00:34:16 +0900

    chore: tanstack query 설치 및 기초 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 60_000, // 1분
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
