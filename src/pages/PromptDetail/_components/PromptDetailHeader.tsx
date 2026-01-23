7121c23f1d398dafb3a6c3001a10b24218c4b15b
ccconac <qwerpoiu91@naver.com>
2026-01-24 01:43:44 +0900

    design: 프롬프트 상세 페이지 헤더 width 속성 추가

--- src/pages/PromptDetail/_components/PromptDetailHeader.tsx
+++ src/pages/PromptDetail/_components/PromptDetailHeader.tsx
@@ -19,7 +19,7 @@ const PromptDetailHeader = ({
   writer,
 }: PromptDetailHeaderProps) => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 w-4xl">
       <div className="flex items-start justify-between gap-4 flex-col">
         <h1 className="text-gray-900 leading-tight flex-1 text-2xl">{title}</h1>
         <div className="flex items-center gap-2 flex-wrap">
