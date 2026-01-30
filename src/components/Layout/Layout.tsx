497534a114ea24a2e5c9c83428a4df50feeecef7
ccconac <qwerpoiu91@naver.com>
2026-01-30 17:21:54 +0900

    feat: 레이아웃 단계에서 메타 데이터 prefetch

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../NavigationBar/NavigationBar';
+import { useMetaOptions } from '../../hooks/common/useMetaOptions';
 
 const Layout = () => {
+  useMetaOptions();
+
   return (
     <div className="flex flex-col mx-auto">
       <NavigationBar />
