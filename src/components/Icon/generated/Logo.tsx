0c42fa567d5c5fe6683b944edd4dcf9bccbc189f
ccconac <qwerpoiu91@naver.com>
2026-02-02 18:35:20 +0900

    move: Logo 아이콘 변경

--- src/components/Icon/generated/Logo.tsx
+++ src/components/Icon/generated/Logo.tsx
@@ -3,19 +3,23 @@ import type { SVGProps } from 'react';
 const SvgLogo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    className="logo_svg__w-8 logo_svg__h-8"
+    viewBox="0 0 32 32"
     width="1em"
     height="1em"
-    fill="none"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth={2}
-    aria-hidden="true"
-    className="logo_svg__lucide logo_svg__lucide-sparkles logo_svg__w-5 logo_svg__h-5 logo_svg__text-white"
-    viewBox="0 0 24 24"
     {...props}>
-    <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594zM20 2v4M22 4h-4" />
-    <circle cx={4} cy={20} r={2} />
+    <path
+      fill="#6D5BD0"
+      d="M28 9a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v10a4 4 0 0 0 4 4h3l5 4 5-4h3a4 4 0 0 0 4-4z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m14 11-4 3 4 3M18 11l4 3-4 3"
+    />
   </svg>
 );
 
