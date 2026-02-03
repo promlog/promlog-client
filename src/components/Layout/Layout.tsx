540c34f31a392055184dd94088b9d353387f623d
ccconac <qwerpoiu91@naver.com>
2026-02-03 20:56:14 +0900

    design: 메인 페이지 제외 레이아웃 디자인 변경

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import { useMetaOptions } from '../../hooks/common/useMetaOptions';
+import { cn } from '../Button/Button.styles';
 
 const Layout = () => {
   useMetaOptions();
+  const location = useLocation();
+
+  const isMainPage = location.pathname === '/';
 
   return (
-    <div className="flex flex-col mx-auto">
+    <div className="flex flex-col mx-auto min-h-screen bg-primary">
       <NavigationBar />
-      <main className="justify-center flex mx-auto max-w-7xl px-4 py-8">
+      <main
+        className={cn(
+          'max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8',
+          !isMainPage &&
+            'h-screen w-7xl bg-white/95 backdrop-blur-sm border-x border-gray-200 shadow-[0_1px_3px_0_rgba(0,0,0,0.05)] flex justify-center'
+        )}>
         <Outlet />
       </main>
     </div>
