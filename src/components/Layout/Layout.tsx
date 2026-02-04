e017fbe692a675d1c90bdf48fb591280d88657b3
ccconac <qwerpoiu91@naver.com>
2026-02-04 16:23:08 +0900

    design: layout 비율 변경

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,13 +10,13 @@ const Layout = () => {
   const isMainPage = location.pathname === '/';
 
   return (
-    <div className="flex flex-col mx-auto min-h-screen bg-primary">
+    <div className="flex-1 flex-col mx-auto min-h-screen bg-primary">
       <NavigationBar />
       <main
         className={cn(
           'max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8',
           !isMainPage &&
-            'h-screen w-7xl bg-white/95 backdrop-blur-sm border-x border-gray-200 shadow-[0_1px_3px_0_rgba(0,0,0,0.05)] flex justify-center'
+            'w-7xl bg-white/95 backdrop-blur-sm border-x border-gray-200 shadow-[0_1px_3px_0_rgba(0,0,0,0.05)] flex justify-center'
         )}>
         <Outlet />
       </main>
